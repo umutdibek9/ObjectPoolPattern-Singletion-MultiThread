118d27dfd4eb5b1ab8637ba1241b0f68ff1aa648
Umut Dibek <fbumut236@gmail.com>
2022-04-14 23:00:39 +0300

    ObjectPoolPattern-Singletion-MultiThread
    C# da objectpoolpattern singletion uygulamasını multthread yardımıyla gerçekleştirme

---
+++ YazılımTasarımKalıpları/YazılımTasarımKalıpları/Client.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace YazılımTasarımKalıpları
+{
+    public abstract class Client
+    {
+        public abstract void Connect(int x);
+    }
+    public class RequestClient : Client
+    {
+        public override void Connect(int x)
+        {
+            var thread = new Thread(() =>
+            {
+                while (true)
+                {
+                    var newClient = ClientPool.Instance.AcquireObject();
+                    if (newClient == null)
+                    {
+                        Console.Write(x + ".Kullanıcı bekletildi\n");
+
+                        Thread.Sleep(2000);
+                    }
+                    else
+                    {
+                        Console.Write(x + ".Kullanıcı erişim yaptı.\n");
+
+                        Thread.Sleep(2000);
+                    }
+                    Thread.Sleep(2000);
+                    if (newClient != null)
+                    {
+                        ClientPool.Instance.ReleaseObject(newClient);
+                        Console.Write(x + ".Kullanıcı erişimi bıraktı.\n");
+
+                        Thread.Sleep(2000);
+                    }
+
+                }
+
+            });
+            thread.Start();
+
+        }
+
+    }
+}
---
+++ YazılımTasarımKalıpları/YazılımTasarımKalıpları/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace YazılımTasarımKalıpları
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clients = new List<RequestClient>();
+            for (int i = 0; i < 15; i++)
+            {
+                clients.Add(new RequestClient());
+            }
+            for (int i = 0; i < 15; i++)
+            {
+                clients[i].Connect(i+1);
+            }
+            
+        }
+    }
+}
